7e624a9046cc659fa0b530be8fd90a661866a340
João Vitor <105259247+JoaoVitorMirandaBandeira@users.noreply.github.com>
2022-12-13 13:23:09 -0300

    Create CheckBox.js

---
+++ CheckBox.js
@@ -0,0 +1,19 @@
+myInterval = setInterval(addColorInstitucional, 1000)
+
+function addColorInstitucional(){
+    for(j = 0; j < document.getElementsByClassName("form-check-input").length ; j++){
+        document.getElementsByClassName('form-check-input')[j].style.accentColor = 'var(--rb-primary-theme-500)' 
+    }
+    function addStyle() {
+        const sheet = new CSSStyleSheet();
+        sheet.insertRule(`formio a {
+            color : var(--rb-primary-theme-500) !important;
+        }`);
+        document.adoptedStyleSheets = [sheet];
+    }
+    addStyle()
+}
+
+function stopAddColorInstitucional(){
+    clearInterval(myInterval);
+}
